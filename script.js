88480c965893dd9fabef4c5522a46cd8ae848cd5
Jake Meyers <jakejake92@yahoo.com>
2020-12-01 18:42:48 -0700

    progress on carosel

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-// hamburger 
-const hamburger = document.querySelector(".hamburger--collapse");
-hamburger.addEventListener("click", () => {
-	hamburger.classList.toggle("is-active");
+// hamburger
+// const hamburger = document.querySelector(".hamburger--collapse");
+// hamburger.addEventListener("click", () => {
+// 	hamburger.classList.toggle("is-active");
+// });
+
+// carasoul
+document.addEventListener("DOMContentLoaded", () => {
+	const testimonails = document.getElementsByClassName("testimonials__content");
+	const itemsLength = testimonails.length;
+	let slide = 0;
+
+	testimonails[0].classList.add("active");
+	testimonails[1].classList.add("next");
+	testimonails[2].classList.add("prev");
 });
 
-// carasoul 
+document.querySelector(".fa-angle-right").addEventListener("click", () => {
+	if (slide === itemsLength - 1) {
+		slide = 0;
+	} else {
+		slide++;
+	}
+
+	testimonails[slide].classList.add("active");
+});
