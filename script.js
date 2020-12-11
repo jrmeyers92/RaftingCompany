75c6f9cd6490f9647055224ef1b8e11d64cbc238
Jake Meyers <jakejake92@yahoo.com>
2020-12-11 14:26:55 -0700

    carosel working for testimonials

--- script.js
+++ script.js
@@ -5,22 +5,53 @@
 // });
 
 // carasoul
-document.addEventListener("DOMContentLoaded", () => {
-	const testimonails = document.getElementsByClassName("testimonials__content");
-	const itemsLength = testimonails.length;
-	let slide = 0;
 
-	testimonails[0].classList.add("active");
-	testimonails[1].classList.add("next");
-	testimonails[2].classList.add("prev");
-});
+(function () {
+	// let testimonials = [
+	// 	{
+	// 		testimonial:
+	// 			"Had an absolute blast! Great streatch of river with lots of exciting rapids. Close to Denver. The entire staff was so friendly and helpful in creating a memoriable expierience! Our guide Mattie was a real pro who was able to deal with our group of inexperienced paddlers for a safe and fun ride. I highly recommend!",
+	// 		author: "Grant A",
+	// 	},
+	// 	{
+	// 		testimonial:
+	// 			"Great summer attaraction close to Denver! Our guide coleman made sure we knew what to do through each of the rapids and kept us from getting too hot on a warm Spring day. The new SKy Trek course looks like tons of fun and I'll definitely be back to try that out as well!",
+	// 		author: "Sarah C",
+	// 	},
+	// 	{
+	// 		testimonial:
+	// 			"Very professional outfitter. Had an excellent experience. Our guid Willie was knowledgeable & made the trip frun for everyone, including our 3 teenage boys. Would reccomend them for anyone looking for whitewater rafging in the area. ",
+	// 		author: "Schilling5",
+	// 	},
+	// ];
 
-document.querySelector(".fa-angle-right").addEventListener("click", () => {
-	if (slide === itemsLength - 1) {
-		slide = 0;
-	} else {
-		slide++;
-	}
+	const slider = document.querySelector(".testimonials__slider");
+	const leftArrow = document.querySelector(".left");
+	const rightArrow = document.querySelector(".right");
 
-	testimonails[slide].classList.add("active");
-});
+	let sectionIndex = 0;
+
+	rightArrow.addEventListener("click", () => {
+		sectionIndex = sectionIndex > 2 ? (sectionIndex = 0) : (sectionIndex += 1);
+		slider.style.transform = "translate(" + sectionIndex * "-25" + "%";
+	});
+
+	leftArrow.addEventListener("click", () => {
+		sectionIndex = sectionIndex > 0 ? (sectionIndex -= 1) : (sectionIndex = 3);
+		slider.style.transform = "translate(" + sectionIndex * "-25" + "%";
+	});
+
+	// const testimonailContainer = document.getElementById("testimonials__content");
+
+	// //set initial testimonial
+
+	// document.addEventListener("DOMContentLoaded", () => {
+	// 	const para = document.createElement("p");
+	// 	para.className = "testimonials__para";
+	// 	para.innerHTML = testimonials[0].testimonial;
+	// 	// const author = document.createElement("span");
+	// 	// author.className = "testimonials__author ";
+	// 	// author.innerHTML = testimonials[0].author;
+	// 	testimonailContainer.appendChild(para);
+	// });
+})();
